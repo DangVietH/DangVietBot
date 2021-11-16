e744a86c81f632957cb48fd9ab283cf8617dd97a
DvH <88995192+DangVietH@users.noreply.github.com>
2021-11-16 16:05:09 +0700

    update level system

--- cogs/levelling.py
+++ cogs/levelling.py
@@ -5,32 +5,32 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 cluster = AsyncIOMotorClient(os.environ.get("mango_link"))
 db = cluster["levelling"]
+role = db['roles']
+xp = db['xp']
 
 
 class Leveling(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.role = db['roles']
-        self.xp = db['xp']
 
     async def update_user(self, user):
-        result = await self.xp.find_one({"guild": user.guild.id})
+        result = await xp.find_one({"guild": user.guild.id})
         if user.guild.id != result['guild'] and user.id != ['member']:
             insert = {"guild": user.guild.id, "member": user.id, "rank": 0, "xp": 0}
-            await self.xp.insert_one(insert)
+            await xp.insert_one(insert)
 
     async def add_exp(self, user):
-        result = await self.xp.find_one({"guild": user.guild.id})
+        result = await xp.find_one({"guild": user.guild.id})
         add_xp = result["xp"] + 5
-        await self.xp.update_one({"guild": user.guild.id}, {"$set": {"xp": add_xp}})
+        await xp.update_one({"guild": user.guild.id}, {"$set": {"xp": add_xp}})
 
     async def level_up(self, user, message):
-        result = await self.xp.find_one({"guild": user.guild.id})
+        result = await xp.find_one({"guild": user.guild.id})
         lvl_og = result['rank']
         lvl_end = int(result['xp'] ** (1/4))
         if lvl_og < lvl_end:
             new_lvl = lvl_og + 1
-            await self.xp.update_one({"guild": user.guild.id}, {"$set": {"rank": new_lvl}})
+            await xp.update_one({"guild": user.guild.id}, {"$set": {"rank": new_lvl}})
             await message.channel.send(f"🎉 {user.mention} has reach level **{new_lvl}**!!")
 
     @commands.Cog.listener()
