b3306524f7c4a04cd3141f542c41fc13636c6230
DvH <88995192+DangVietH@users.noreply.github.com>
2021-11-17 14:32:16 +0700

    can this work?

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -14,25 +14,25 @@ class Leveling(commands.Cog):
         self.xp = db['xp']
 
     async def update_user(self, user, message):
-        result = await self.xp.find_one({"guild": message.guild.id})
-        if message.guild.id != result['guild'] and user.id != result['member']:
+        result = await self.xp.find_one({"guild": message.guild.id}, {"member": user.id})
+        if result is None:
             insert = {"guild": message.guild.id, "member": user.id, "rank": 0, "xp": 0}
             await self.xp.insert_one(insert)
 
     async def add_exp(self, user, message):
-        result = await self.xp.find_one({"guild": message.guild.id})
-        if result['member'] == user.id:
+        result = await self.xp.find_one({"guild": message.guild.id}, {"member": user.id})
+        if result is not None:
             add_xp = result["xp"] + 5
-            await self.xp.update_one({"guild": message.guild.id}, {"$set": {"xp": add_xp}})
+            await self.xp.update_one({"guild": message.guild.id}, {"member": user.id}, {"$set": {"xp": add_xp}})
 
     async def level_up(self, user, message):
-        result = await self.xp.find_one({"guild": message.guild.id})
-        if result['member'] == user.id:
+        result = await self.xp.find_one({"guild": message.guild.id}, {"member": user.id})
+        if result is not None:
             lvl_og = result['rank']
             lvl_end = int(result['xp'] ** (1 / 4))
             if lvl_og < lvl_end:
                 new_lvl = lvl_og + 1
-                await self.xp.update_one({"guild": message.guild.id}, {"$set": {"rank": new_lvl}})
+                await self.xp.update_one({"guild": message.guild.id}, {"member": user.id}, {"$set": {"rank": new_lvl}})
                 await message.channel.send(f"🎉 {user.mention} has reach level **{new_lvl}**!!")
 
     @commands.Cog.listener()
@@ -46,8 +46,8 @@ class Leveling(commands.Cog):
     @commands.command(help="See your rank")
     async def rank(self, ctx, member: discord.Member = None):
         member = member or ctx.author
-        result = await self.xp.find_one({"guild": member.guild.id})
-        if result['member'] == member.id:
+        result = await self.xp.find_one({"guild": member.guild.id}, {"member": member.id})
+        if result is not None:
             embed = discord.Embed(title=member, color=member.color)
             embed.add_field(name="Level", value=f"#{result['rank']}")
             embed.add_field(name="XP", value=f"#{result['xp']}")
