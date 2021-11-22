3bb7451f787e424d31c8951c423e0c839e53e0fe
DvH <88995192+DangVietH@users.noreply.github.com>
2021-11-22 09:10:33 +0700

    update ranl

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -17,15 +17,15 @@ class Leveling(commands.Cog):
     async def on_message(self, message):
         if message.guild:
             if not message.author.bot:
-                result = await member.find_one({'guild': message.guild.id, "user": message.author.id})
-                if result is None:
+                stats = await member.find_one({'guild': message.guild.id, "user": message.author.id})
+                if stats is None:
                     insert = {'guild': message.guild.id, "user": message.author.id, 'level': 0, 'xp': 0}
                     await member.insert_one(insert)
                 else:
-                    add_exp = result['xp'] + 5
+                    add_exp = stats['xp'] + 5
                     await member.update_one({"guild": message.guild.id, "user": message.author.id}, {"$set": {"xp": add_exp}})
-                    lvl_start = result['level']
-                    lvl_end = int(result['xp'] ** (1 / 4))
+                    lvl_start = stats['level']
+                    lvl_end = int(stats['xp'] ** (1 / 4))
                     if lvl_start < lvl_end:
                         new_lvl = lvl_start + 1
                         await member.update_one({"guild": message.guild.id, "user": message.author.id}, {"$set": {"level": new_lvl}})
@@ -34,12 +34,25 @@ class Leveling(commands.Cog):
     @commands.command(help="See your rank")
     async def rank(self, ctx, user: discord.Member = None):
         user = user or ctx.author
-        result = await member.find_one({'guild': ctx.guild.id, "user": user.id})
-        if result is not None:
-            embed = discord.Embed(title=user, color=user.color)
-            embed.add_field(name="Level", value=f"#{result['level']}")
-            embed.add_field(name="XP", value=f"#{result['xp']}")
-            embed.set_thumbnail(url=user.avatar.url)
+        stats = await member.find_one({'guild': ctx.guild.id, "user": user.id})
+        if stats is not None:
+            xp = stats["xp"]
+            lvl = stats["level"]
+            rank = 0
+            xp -= ((50 * ((lvl - 1) ** 2)) + (50 * (lvl - 1)))
+            boxes = int((xp / (200 * ((1 / 2) * lvl))) * 20)  # shows boxes (for visual effect)
+            rankings = member.find({"guild": ctx.guild.id}).sort("xp", -1)
+            for x in rankings:  # to show what rank they are
+                rank += 1
+                if stats["user"] == x["user"]:
+                    break
+            # using this to send all the info
+            embed = discord.Embed(title="{}'s level stats".format(ctx.author.name))
+            embed.add_field(name="XP", value=f"{xp}/{int(200 * ((1 / 2) * lvl))}", inline=True)
+            embed.add_field(name="Rank", value=f"{rank}/{ctx.guild.member_count}", inline=True)
+            embed.add_field(name="Progress",
+                            value=boxes * ":red_square:" + (20 - boxes) * ":black_large_square:", inline=False)
+            embed.set_thumbnail(url=ctx.author.avatar.url)
             await ctx.send(embed=embed)
         else:
             await ctx.send(f"The specified member haven't send a message in this server!!")
