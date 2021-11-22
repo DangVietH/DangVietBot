f4780d6aab88affd73db2b8fc66897f5ac61cd7d
DvH <88995192+DangVietH@users.noreply.github.com>
2021-11-22 10:26:42 +0700

    return to monke for now

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from easy_pil import Editor, Canvas, load_image_async, Font
 
 cluster = AsyncIOMotorClient(os.environ.get("mango_link"))
 db = cluster["levelling"]
@@ -37,51 +36,11 @@ class Leveling(commands.Cog):
         user = user or ctx.author
         stats = await member.find_one({'guild': ctx.guild.id, "user": user.id})
         if stats is not None:
-            next_level_xp = (stats["level"] + 1) * 100
-            current_level_xp = stats["level"] * 100
-            xp_need = next_level_xp - current_level_xp
-            xp_have = stats["xp"] - current_level_xp
-
-            percentage = (xp_need / 100) * xp_have
-
-            background = Editor(Canvas((934, 282), "#23272a"))
-            profile = await load_image_async(str(member.avatar.url))
-
-            profile = Editor(profile).resize((150, 150)).circle_image()
-
-            poppins = Font().poppins(size=40)
-            poppins_small = Font().poppins(size=30)
-
-            square = Canvas((500, 500), "#06FFBF")
-            square = Editor(square)
-            square.rotate(30, expand=True)
-
-            background.paste(square.image, (600, -250))
-            background.paste(profile.image, (30, 30))
-
-            background.rectangle((30, 220), width=650, height=40, fill="white", radius=20)
-            background.bar(
-                (30, 220),
-                max_width=650,
-                height=40,
-                percentage=percentage,
-                fill="#FF56B2",
-                radius=20,
-            )
-            background.text((200, 40), str(member), font=poppins, color="white")
-
-            background.rectangle((200, 100), width=350, height=2, fill="#17F3F6")
-            background.text(
-                (200, 130),
-                f"Level : {stats['level']}"
-                + f" XP : {stats['xp']} / {(stats['level'] + 1) * 100}",
-                font=poppins_small,
-                color="white",
-            )
-
-            file = discord.File(fp=background.image_bytes, filename="rank.png")
-            await ctx.send(file=file)
-
+            embed = discord.Embed(title=user, color=user.color)
+            embed.add_field(name="Level", value=f"#{stats['level']}")
+            embed.add_field(name="XP", value=f"#{stats['xp']}")
+            embed.set_thumbnail(url=user.avatar.url)
+            await ctx.send(embed=embed)
         else:
             await ctx.send(f"The specified member haven't send a message in this server!!")
 
