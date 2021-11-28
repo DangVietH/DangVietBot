543d2028f52fc1983189ef9c7730e9eaf442f8f1
DvH <88995192+DangVietH@users.noreply.github.com>
2021-11-28 10:34:27 +0700

    destroy your meeting

---
+++ cogs/audio/soundboard.py
@@ -0,0 +1,27 @@
+import discord
+from discord import FFmpegPCMAudio
+import asyncio
+from discord.ext import commands
+
+NOT_CONNECTED_TO_VOICE = "You're not connected to a vc"
+
+
+class SoundBoard(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(help="Ruin your boss meeting with this")
+    async def airhorn(self, ctx):
+        if not ctx.author.voice:
+            await ctx.send(NOT_CONNECTED_TO_VOICE)
+        else:
+            ctx.message.add_reaction("🔊")
+            vc = await ctx.author.voice.channel.connect()
+            vc.play(FFmpegPCMAudio(source="./sounds/mlg-airhorn.mp3"))
+            while vc.is_playing():
+                await asyncio.sleep(1)
+            await vc.disconnect()
+
+
+def setup(bot):
+    bot.add_cog(SoundBoard(bot))
