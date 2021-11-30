83f03f10f5507d4ef1c87c3e5aa986b3f3511f76
DvH <88995192+DangVietH@users.noreply.github.com>
2021-11-30 16:08:11 +0700

    sync

--- cogs/economy/economy.py
+++ cogs/economy/economy.py
@@ -179,7 +179,7 @@ class Economy(commands.Cog):
                                         }, upsert=True)
 
                 # if exist
-                await cursor.update_one({"id": user.id, "inventory.name": str(item_name)}, {"$set": {"inventory.$.amount": int(amount)}})
+                await cursor.update_one({"id": user.id, "inventory.name": str(item_name)}, {"$inc": {"inventory.$.amount": int(amount)}})
 
                 # get ye money
                 newBal = wallet - cost
