b0e59037ef5906f69177ff0345d6aa50120d17b4
DvH <88995192+DangVietH@users.noreply.github.com>
2021-12-06 16:13:11 +0700

    add modlog

--- cogs/moderation/admin.py
+++ cogs/moderation/admin.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+cluster = AsyncIOMotorClient(os.environ.get("mango_link"))
+modlogdb = cluster["moderation"]
+cursors = modlogdb['modlog']
+cases = modlogdb['cases']
 
 
 class Admin(commands.Cog):
@@ -21,6 +28,16 @@ class Admin(commands.Cog):
             emb = discord.Embed(description=f"You have been warned in **{guild.name}** for: **{reason}**", color=discord.Color.red())
             await member.send(embed=emb)
 
+            await cases.update_one({"guild": ctx.guild.id}, {"$inc": {"num": 1}})
+            num_of_case = await cases.find_one({"guild": ctx.guild.id})['num']
+            await cases.update_one({"guild": ctx.guild.id}, {"$push": {"cases": {"Number": int(num_of_case), "type": "warning", "reason": str(reason)}}})
+
+            result = await cursors.find_one({"guild": ctx.guild.id})
+            if result is not None:
+                channel = self.bot.get_channel(result["channel"])
+                embed = discord.Embed(title=f"Case #{num_of_case}: Warn!", description=f"Reason: {reason}", color=discord.Color.red())
+                await channel.send(embed=embed)
+
     @commands.command(help="Mute member")
     @commands.has_permissions(administrator=True)
     async def mute(self, ctx, member: discord.Member, *, reason=None):
@@ -38,6 +55,17 @@ class Admin(commands.Cog):
         await member.add_roles(mutedRole, reason=reason)
         await ctx.send(f"Muted {member.mention} for reason {reason}")
         await member.send(f"You were muted in **{guild.name}** for {reason}")
+
+        await cases.update_one({"guild": ctx.guild.id}, {"$inc": {"num": 1}})
+        num_of_case = await cases.find_one({"guild": ctx.guild.id})['num']
+        await cases.update_one({"guild": ctx.guild.id},
+                               {"$push": {"cases": {"Number": int(num_of_case), "type": "mute", "reason": str(reason)}}})
+
+        result = await cursors.find_one({"guild": ctx.guild.id})
+        if result is not None:
+            channel = self.bot.get_channel(result["channel"])
+            embed = discord.Embed(title=f"Case #{num_of_case}: Mute!", description=f"Reason: {reason}", color=discord.Color.red())
+            await channel.send(embed=embed)
 
     @commands.command(help="Unmute member")
     @commands.has_permissions(administrator=True)
@@ -57,6 +85,17 @@ class Admin(commands.Cog):
         await member.kick(reason=reason)
         await ctx.send(f'{member.mention} has been kick for {reason}')
 
+        await cases.update_one({"guild": ctx.guild.id}, {"$inc": {"num": 1}})
+        num_of_case = await cases.find_one({"guild": ctx.guild.id})['num']
+        await cases.update_one({"guild": ctx.guild.id},
+                               {"$push": {"cases": {"Number": int(num_of_case), "type": "kick", "reason": str(reason)}}})
+
+        result = await cursors.find_one({"guild": ctx.guild.id})
+        if result is not None:
+            channel = self.bot.get_channel(result["channel"])
+            embed = discord.Embed(title=f"Case #{num_of_case}: Kick!", description=f"Reason: {reason}", color=discord.Color.red())
+            await channel.send(embed=embed)
+
     @commands.command(help="Ban member")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
@@ -64,6 +103,17 @@ class Admin(commands.Cog):
             await member.send(f"You've been **BANNED** from **{ctx.guild.name}** for {reason}. What a shame 👎")
         await member.ban(reason=reason)
         await ctx.send(f'{member.mention} has been **BANNED** for {reason}')
+
+        await cases.update_one({"guild": ctx.guild.id}, {"$inc": {"num": 1}})
+        num_of_case = await cases.find_one({"guild": ctx.guild.id})['num']
+        await cases.update_one({"guild": ctx.guild.id},
+                               {"$push": {"cases": {"Number": int(num_of_case), "type": "ban", "reason": str(reason)}}})
+
+        result = await cursors.find_one({"guild": ctx.guild.id})
+        if result is not None:
+            channel = self.bot.get_channel(result["channel"])
+            embed = discord.Embed(title=f"Case #{num_of_case}: Ban!", description=f"Reason: {reason}", color=discord.Color.red())
+            await channel.send(embed=embed)
 
     @commands.command(help="Unban member")
     @commands.has_permissions(administrator=True)
@@ -94,3 +144,46 @@ class Admin(commands.Cog):
         channel = channel or ctx.channel
         await channel.set_permissions(ctx.guild.default_role, send_messages=True)
         await ctx.send('Channel unlocked.')
+
+    @commands.group(invoke_without_command=True, help="Modlog and case")
+    async def modlog(self, ctx):
+        await ctx.invoke(self.bot.get_command('help'), command_or_cog='tag')
+
+    @modlog.command(help="Set up channel")
+    @commands.has_permissions(administrator=True)
+    async def channel(self, ctx, channel: discord.TextChannel):
+        result = await cursors.find_one({"guild": ctx.guild.id})
+        if result is None:
+            if result is None:
+                insert = {"guild": ctx.guild.id, "channel": channel.id}
+                await cursors.insert_one(insert)
+                await ctx.send(f"Modlog channel set to {channel.mention}")
+            elif result is not None:
+                await cursors.update_one({"guild": ctx.guild.id}, {"$set": {"channel": channel.id}})
+                await ctx.send(f"Modlog channel updated to {channel.mention}")
+
+    @modlog.command(help="Remove modlog channel if you like to")
+    @commands.has_permissions(administrator=True)
+    async def remove(self, ctx):
+        result = await cursors.find_one({"guild": ctx.guild.id})
+        if result is not None:
+            await cursors.delete_one(result)
+            await ctx.send("Modlog channel has been remove")
+        else:
+            await ctx.send("You don't have a Modlog channel")
+
+    @commands.command(help="Look at your server cases")
+    async def caselist(self, ctx):
+        results = await cases.find_one({"guild": ctx.guild.id})
+        total_cases = results['cases']
+        if len(total_cases) < 1:
+            await ctx.send("Looks like all your server members are good people 🥰")
+        embed = discord.Embed(title=f"{ctx.guild.name} caselist", description=f"Total case: {results['num']}", color=discord.Color.red())
+        for case in total_cases:
+            embed.add_field(name=f"Case {case['Number']}", value=f"**Type:** {case['type']}\n**Reason:** {case['reason']}")
+        await ctx.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        insert = {"guild": guild.id, "num": 0, "cases": []}
+        await cursors.insert_one(insert)
