e5ce6cacf395112984d779ebca6977f3d8d62190
DvH <88995192+DangVietH@users.noreply.github.com>
2021-12-11 20:18:09 +0700

    it fix

--- cogs/moderation/admin.py
+++ cogs/moderation/admin.py
@@ -84,7 +84,7 @@ class Admin(commands.Cog):
             await user_case.update_one({"guild": ctx.guild.id, "user": member.id}, {"$inc": {"total_cases": 1}})
 
     @commands.command(help="Mute member but with a timer")
-    @commands.is_owner()
+    @commands.has_permissions(administrator=True)
     async def tempmute(self, ctx, member: discord.Member, time, *, reason=None):
         def convert(time):
             pos = ['s', 'm', 'h', 'd']
