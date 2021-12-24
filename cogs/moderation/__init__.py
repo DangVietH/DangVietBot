c71ef90ccc6a3224741328bab389c2af8cc87611
DvH <88995192+DangVietH@users.noreply.github.com>
2021-12-24 16:09:42 +0700

    we always fogot stuff

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -1,9 +1,7 @@
 from .admin import Admin
 from .giveaway import Giveaway
-from .automod import Automods
 
 
 def setup(bot):
     bot.add_cog(Admin(bot))
     bot.add_cog(Giveaway(bot))
-    bot.add_cog(Automods(bot))
