62b8f12da53a81f1f310269c96a830da775a7232
DvH <88995192+DangVietH@users.noreply.github.com>
2021-12-30 09:05:49 +0700

    add automod and stuff

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -1,9 +1,11 @@
 from .admin import Admin
 from .giveaway import Giveaway
 from .automod import AutoMod
+froom automod_event import AutoModEvent
 
 
 def setup(bot):
     bot.add_cog(Admin(bot))
     bot.add_cog(Giveaway(bot))
     bot.add_cog(AutoMod(bot))
+    bot.add_cog(AutoModEvent(bot))
