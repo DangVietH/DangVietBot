940fc8d89dee11f36190d35f696e01bf8a33ad35
DvH <88995192+DangVietH@users.noreply.github.com>
2021-12-30 13:39:45 +0700

    back to boomer

--- cogs/moderation/automod.py
+++ cogs/moderation/automod.py
@@ -13,40 +13,13 @@ class AutoMod(commands.Cog):
         self.antispam = commands.CooldownMapping.from_cooldown(10, 12.0, commands.BucketType.user)
 
     @commands.Cog.listener(name="on_message")
-    async def when_automod(self, message: discord.Message):
+    async def is_blacklist(self, message: discord.Message):
         if message.guild:
             check = await cursor.find_one({"guild": message.guild.id})
             if check is None:
                 return
             else:
-                if message.author.bot:
-                    return
-                elif not isinstance(message.author, discord.Member):
-                    return
-                elif message.author.guild_permissions.administrator:
-                    return
-                else:
-                    await self.is_blacklist(message)
-
-    @commands.Cog.listener(name="on_message_edit")
-    async def when_automod_is_edit(self, before, after):
-        message = after
-        if message.guild:
-            check = await cursor.find_one({"guild": message.guild.id})
-            if check is None:
-                return
-            else:
-                if message.author.bot:
-                    return
-                elif not isinstance(message.author, discord.Member):
-                    return
-                elif message.author.guild_permissions.administrator:
-                    return
-                else:
-                    await self.is_blacklist(message)
-
-    async def is_blacklist(self, message):
-        check = await cursor.find_one({"guild": message.guild.id, "blacklist": message.content.lower()})
-        if check is not None:
-            await message.delete()
-            await message.author.send("That's a blacklisted word")
+                check2 = await cursor.find_one({"guild": message.guild.id, "blacklist": message.content.lower()})
+                if check2 is not None:
+                    await message.delete()
+                    await message.author.send("That's a blacklisted word")
