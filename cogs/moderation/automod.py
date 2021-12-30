6172ecb465f1572919d9718c69959c59313d1685
DvH <88995192+DangVietH@users.noreply.github.com>
2021-12-30 14:06:31 +0700

    again?

--- cogs/moderation/automod.py
+++ cogs/moderation/automod.py
@@ -23,27 +23,3 @@ class AutoMod(commands.Cog):
                 if check2 is not None:
                     await message.delete()
                     await message.author.send("That's a blacklisted word")
-
-    @commands.Cog.listener(name="on_message")
-    async def anti_invite(self, message: discord.Message):
-        if message.guild:
-            check = await cursor.find_one({"guild": message.guild.id})
-            if check is None:
-                return
-            else:
-                if "discord.gg" or "discord.com/invite" in message.content.lower():
-                    if check['anti invite'] == "on":
-                        await message.delete()
-                        await message.author.send("You cannot send invite links in this server")
-
-    @commands.Cog.listener(name="on_message")
-    async def anti_mass_ping(self, message: discord.Message):
-        if message.guild:
-            check = await cursor.find_one({"guild": message.guild.id})
-            if check is None:
-                return
-            else:
-                if len(message.mentions) > 3:
-                    if check['anti mass mention'] == "on":
-                        await message.delete()
-                        await message.channel.send("There's a mass ping. Do something mods")
