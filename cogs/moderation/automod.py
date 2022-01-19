925afb42a0a8830337c812e37f57edf13e71a8b3
DvH <88995192+DangVietH@users.noreply.github.com>
2022-01-19 14:19:32 +0700

    use regrex

--- cogs/moderation/automod.py
+++ cogs/moderation/automod.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 from motor.motor_asyncio import AsyncIOMotorClient
 from main import config_var
-from datetime import datetime
+from datetime import timezone, datetime
+import asyncio
+import re
 
 cluster = AsyncIOMotorClient(config_var['mango_link'])
 cursor = cluster["moderation"]['automod']
@@ -13,66 +15,55 @@ mchannel = cluster["moderation"]['modlog']
 class AutoMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.antispam = commands.CooldownMapping.from_cooldown(10, 12.0, commands.BucketType.user)
+        self.invite = re.compile(r'discord(?:\.com/invite|app\.com/invite|\.gg)/?([a-zA-Z0-9\-]{2,32})')
+        self.links = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|%[0-9a-fA-F][0-9a-fA-F])+")
 
-    @commands.Cog.listener(name="on_message")
-    async def is_blacklist(self, message: discord.Message):
+    @commands.Cog.listener()
+    async def on_message(self, message):
         if message.guild:
-            check = await cursor.find_one({"guild": message.guild.id})
-            if check is None:
-                return
+            data = await cursor.find_one({"guild": message.guild.id})
+            if data is None: return
             else:
-                check2 = await cursor.find_one({"guild": message.guild.id, "blacklist": message.content.lower()})
-                if check2 is not None:
-                    await message.delete()
-                    await message.author.send("That's a blacklisted word")
-
-    @commands.Cog.listener(name="on_message")
-    async def anti_invite(self, message: discord.Message):
-        if message.guild:
-            if not message.author.bot:
-                check = await cursor.find_one({"guild": message.guild.id})
-                if check is None:
-                    return
+                if message.author.bot or message.author.guild_permissions.administrator is True: return
                 else:
-                    if "discord.gg" in message.content.lower():
-                        if check['anti invite'] == "on":
-                            await message.delete()
-                            await message.author.send("You cannot send invite links in this server")
-                    elif "discord.com/invite" in message.content.lower():
-                        if check['anti invite'] == "on":
-                            await message.delete()
-                            await message.author.send("You cannot send invite links in this server")
+                    check2 = await cursor.find_one({"guild": message.guild.id, "blacklist": message.content.lower()})
+                    if check2 is not None:
+                        await message.delete()
+                        await message.author.send("That's a blacklisted word")
+                    # anti invite 
+                    if self.invite.findall(message.content) is True and data['anti invite'] == "on":
+                        await message.delete()
+                        await message.author.send("You cannot send invite links in this server")
+                
+                    # anti spam 
+                    if data['anti spam'] == "on":
+                        def check (message): return (message.author == message.author and (datetime.now(timezone.utc) - message.created_at).seconds < 15)
+                        if message.author.guild_permissions.administrator: return 
+                        if len(list(filter(lambda m: check(m), self.bot.cached_messages))) >= 8:
+                            mutedRole = discord.utils.get(message.guild.roles, name="DHB_muted")
+                            if not mutedRole:
+                                mutedRole = await message.guild.create_role(name="DHB_muted")
 
-    @commands.Cog.listener(name="on_message")
-    async def anti_link(self, message: discord.Message):
-        if message.guild:
-            if not message.author.bot:
-                check = await cursor.find_one({"guild": message.guild.id})
-                if check is None:
-                    return
-                else:
-                    if "https://" in message.content.lower():
-                        if check['anti link'] == "on":
-                            await message.delete()
-                            await message.author.send("You cannot send links in this server")
-                    elif "http://" in message.content.lower():
-                        if check['anti link'] == "on":
-                            await message.delete()
-                            await message.author.send("You cannot send links in this server")
-
-    @commands.Cog.listener(name="on_message")
-    async def anti_mass_ping(self, message: discord.Message):
-        if message.guild:
-            if not message.author.bot:
-                check = await cursor.find_one({"guild": message.guild.id})
-                if check is None:
-                    return
-                else:
-                    if len(message.mentions) > 3:
-                        if check['anti mass mention'] == "on":
-                            await message.delete()
-                            await message.channel.send("There's a mass ping. Do something mods")
+                                for channel in message.guild.channels:
+                                    await channel.set_permissions(mutedRole,
+                                              speak=False,
+                                              send_messages=False,
+                                              read_message_history=True,
+                                              read_messages=False)
+                            await message.author.add_roles(mutedRole, reason="spam")
+                            await message.channel.purge(limit=8)
+                            await message.channel.send(f"Temporaly mute {message.author.mention} for spamming", delete_after=5)
+                            await asyncio.sleep(60)
+                            await message.author.send("I unmuted you. Now BEHAVE OK!")
+                
+                    # anti link
+                    if not self.invite.findall(message.content) is True and self.links.findall(message.content) and data['anti link'] == "on":
+                        await message.delete()
+                        await message.author.send("You cannot send links in this server")
+                
+                    if len(message.mentions) > 3 and data['anti mass mention'] == "on":
+                        await message.delete()
+                        await message.channel.send("There's a mass ping. Do something mods")   
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -83,7 +74,7 @@ class AutoMod(commands.Cog):
             if check['anti alt'] == "on":
                 if not member.bot:
                     created = member.created_at
-                    now = datetime.now()
+                    now = datetime.now(timezone.utc)
                     delta = (now - created).days
                     if delta <= 10:
                         num_of_case = (await cases.find_one({"guild": member.guild.id}))['num'] + 1
@@ -100,5 +91,5 @@ class AutoMod(commands.Cog):
                                                   color=discord.Color.red())
                             await channel.send(embed=embed)
 
-                        await member.send("It's seems you're an alt by your account age!")
+                        await member.send("Because of your account age, you're kicked because you can be an alt!")
                         await member.kick(reason="Potential alt")
