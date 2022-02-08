92c2eb09ce4b45aab10064ec1ceaf16e23d99158
DvH <88995192+DangVietH@users.noreply.github.com>
2022-02-08 20:35:36 +0700

    fix link regrex

--- cogs/moderation/automod.py
+++ cogs/moderation/automod.py
@@ -15,16 +15,18 @@ mchannel = cluster["moderation"]['modlog']
 class AutoMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.invite = re.compile(r'discord(?:\.com/invite|app\.com/invite|\.gg)/?([a-zA-Z0-9\-]{2,32})')
-        self.links = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|%[0-9a-fA-F][0-9a-fA-F])+")
+        self.invite = re.compile(r'((http(s|):\/\/|)(discord)(\.(gg|io|me)\/|app\.com\/invite\/)([0-z]+))')
+        self.links = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.guild:
             data = await cursor.find_one({"guild": message.guild.id})
-            if data is None: return
+            if data is None:
+                return
             else:
-                if message.author.bot or message.author.guild_permissions.administrator is True: return
+                if message.author.bot or message.author.guild_permissions.administrator is True:
+                    return
                 else:
                     check2 = await cursor.find_one({"guild": message.guild.id, "blacklist": message.content.lower()})
                     if check2 is not None:
@@ -35,38 +37,42 @@ class AutoMod(commands.Cog):
                         if self.invite.findall(message.content) is True:
                             await message.delete()
                             await message.author.send("You cannot send invite links in this server")
-                
+
                     # anti spam 
                     if data['anti spam'] == "on":
-                        def check (message): return (message.author == message.author and (datetime.now(timezone.utc) - message.created_at).seconds < 15)
-                        if message.author.guild_permissions.administrator: return 
-                        if len(list(filter(lambda m: check(m), self.bot.cached_messages))) >= 8:
+                        def check(m):
+                            return m.author == message.author and (datetime.utcnow() - m.created_at.replace(tzinfo=None)).seconds < 5
+
+                        if message.author.guild_permissions.administrator:
+                            return
+                        if len(list(filter(lambda m: check(m), self.bot.cached_messages))) >= 3:
                             mutedRole = discord.utils.get(message.guild.roles, name="DHB_muted")
                             if not mutedRole:
                                 mutedRole = await message.guild.create_role(name="DHB_muted")
 
                                 for channel in message.guild.channels:
                                     await channel.set_permissions(mutedRole,
-                                              speak=False,
-                                              send_messages=False,
-                                              read_message_history=True,
-                                              read_messages=False)
+                                                                  speak=False,
+                                                                  send_messages=False,
+                                                                  read_message_history=True,
+                                                                  read_messages=False)
                             await message.author.add_roles(mutedRole, reason="spam")
                             await message.channel.purge(limit=8)
-                            await message.channel.send(f"Temporaly mute {message.author.mention} for spamming", delete_after=5)
+                            await message.channel.send(f"Temporally mute {message.author.mention} for spamming",
+                                                       delete_after=5)
                             await asyncio.sleep(60)
                             await message.author.send("I unmuted you. Now BEHAVE OK!")
-                
+
                     # anti link
                     if data['anti link'] == "on":
-                        if self.links.findall(message.content):
+                        if re.findall(self.links, message.content) is True:
                             await message.delete()
                             await message.author.send("You cannot send links in this server")
-                
+
                     if data['anti mass mention'] == "on":
-                        if len(message.mentions) > 3:
+                        if len(message.mentions) >= 3:
                             await message.delete()
-                            await message.channel.send("There's a mass ping. Do something mods")   
+                            await message.channel.send("There's a mass ping. Do something mods")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
