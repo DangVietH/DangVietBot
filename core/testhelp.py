72dc697b0eeb249961b23fc90493e0dae59dc41a
DvH <88995192+DangVietH@users.noreply.github.com>
2022-03-10 18:09:04 +0700

    sum menu help commands

--- core/testhelp.py
+++ core/testhelp.py
@@ -2,6 +2,8 @@ import nextcord as discord
 from nextcord.ext import commands, menus
 from utils.menuUtils import MenuButtons
 
+# menu help base on https://github.com/DenverCoder1/dev-pro-tips-bot/blob/main/cogs/help/help_command.py
+
 
 class HelpPageSource(menus.ListPageSource):
     def __init__(self, hdata, data):
@@ -9,12 +11,11 @@ class HelpPageSource(menus.ListPageSource):
         super().__init__(data, per_page=1)
 
     async def format_page(self, menu, entries):
-        embed = discord.Embed(
-            title=f"Use {self.helpcommand.context.clean_prefix}help [something] for more info on a command or category.",
-            color=discord.Color.from_rgb(225, 0, 92)
-        )
+        embed = discord.Embed(color=discord.Color.from_rgb(225, 0, 92))
+        embed.description = f"Use {self.helpcommand.clean_prefix}help [something] for more info on a command or category."
         embed.set_author(name=f"DangVietBot Help", icon_url="https://cdn.discordapp.com/avatars/875589545532485682/a5123a4fa15dad3beca44144d6749189.png?size=1024")
-        embed.description = "\n".join([f"{name}: {value}" for name, value in entries])
+        for entry in entries:
+            embed.add_field(name=entry[0], value=entry[1])
         embed.set_footer(text=f'Page {menu.current_page + 1}/{self.get_max_pages()}')
         return embed
 
@@ -24,23 +25,17 @@ class CustomHelp(commands.HelpCommand):
         return f'{self.context.clean_prefix}{command.qualified_name} {command.signature}'
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title='DangVietBot Commands',
-                              description=f"{self.context.bot.description}",
-                              color=discord.Color.from_rgb(225, 0, 92))
+        data = []
         for cog, command in mapping.items():
             name = 'No Category' if cog is None else cog.qualified_name
             filtered = await self.filter_commands(commands, sort=True)
             if filtered:
-                value = '\u2002'.join(c.name for c in commands)
+                value = '\u2002'.join(f"- {self.context.clean_prefix}{c.name}: {c.help}" for c in filtered)
                 if cog and cog.description:
-                    value = '{0}\n{1}'.format(cog.description, value)
+                    data.append((f"**{cog} Commands**", value))
 
-                embed.add_field(name=name, value=value)
-        embed.set_footer(text=f'Use {self.context.clean_prefix}help [something] for more info on a command or category. \nExample: {self.context.clean_prefix}help Economy')
-        view = discord.ui.View()
-        view.add_item(discord.ui.Button(label='Invite', url='https://bit.ly/3BJa6Nj'))
-        view.add_item(discord.ui.Button(label='My server', url='https://discord.gg/cnydBRnHU9'))
-        await self.get_destination().send(embed=embed, view=view)
+        page = MenuButtons(source=HelpPageSource(self.context, data), disable_buttons_after=True, ctx=self.context)
+        await page.start(self.context)
 
     async def send_cog_help(self, cog_):
         embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog_), color=discord.Color.from_rgb(225, 0, 92),
