d7428b2667c1a3d8f2f9b5b842903301cde8c32b
DvH <88995192+DangVietH@users.noreply.github.com>
2022-03-11 12:54:59 +0700

    probaly

--- core/testhelp.py
+++ core/testhelp.py
@@ -7,12 +7,12 @@ from utils.menuUtils import MenuButtons
 
 class HelpPageSource(menus.ListPageSource):
     def __init__(self, hdata, data):
-        self._helpcommand = hdata
+        self._helpcmds = hdata
         super().__init__(data, per_page=1)
 
     async def format_page(self, menu, entries):
         embed = discord.Embed(color=discord.Color.from_rgb(225, 0, 92))
-        embed.description = f"Use {self._helpcommand.context.clean_prefix}help [something] for more info on a command or category."
+        embed.description = f"Use {self._helpcmds.context.clean_prefix}help [something] for more info on a command or category."
         embed.set_author(name=f"DangVietBot Help", icon_url="https://cdn.discordapp.com/avatars/875589545532485682/a5123a4fa15dad3beca44144d6749189.png?size=1024")
         for entry in entries:
             embed.add_field(name=entry[0], value=entry[1])
@@ -20,9 +20,9 @@ class HelpPageSource(menus.ListPageSource):
         return embed
 
 
-class DangVietHelp(commands.HelpCommand):
+class CustomHelp(commands.HelpCommand):
     def get_command_signature(self, command):
-        return f'{self.context.clean_prefix}{command.qualified_name} {command.signature}'
+        return f'- {self.context.clean_prefix}{command.qualified_name} : {command.signature}'
 
     async def send_bot_help(self, mapping):
         data = []
@@ -30,7 +30,7 @@ class DangVietHelp(commands.HelpCommand):
             name = 'No Category' if cog is None else cog.qualified_name
             filtered = await self.filter_commands(commands, sort=True)
             if filtered:
-                value = '\u2002'.join(f"- {self.context.clean_prefix}{c.name} : {c.short_doc}" for c in filtered)
+                value = '\u2002'.join(self.get_command_signature(c) for c in filtered)
                 if cog and cog.description:
                     data.append((f"**{name} Commands**", value))
 
