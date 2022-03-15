42c6eb147602e5c4dd07091a4f0a07bf3ad2e9b9
DvH <88995192+DangVietH@users.noreply.github.com>
2022-03-15 14:36:53 +0700

    it should be nextcord

--- cogs/utilities/info.py
+++ cogs/utilities/info.py
@@ -80,7 +80,7 @@ class Info(commands.Cog):
         embed = discord.Embed(title="Bot information", color=discord.Color.random())
         embed.add_field(name="Developer", value=f"DvH#9980")
         embed.add_field(name="Written in", value="Python 3.10.1")
-        embed.add_field(name="Library", value="[discord.py 2.0](https://github.com/Rapptz/discord.py)")
+        embed.add_field(name="Library", value="[nextcord 2.0](https://github.com/nextcord/nextcord)")
         embed.add_field(name="Create at", value="8/13/2021")
         embed.add_field(name="Command's", value=f"{len(self.bot.commands)}")
         embed.add_field(name="Server's", value=f"{len(self.bot.guilds)}")
