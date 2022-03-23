83797d392c48d392a9d1f6e4e92561972fb8d38a
DvH <88995192+DangVietH@users.noreply.github.com>
2022-03-23 11:04:46 +0700

    avoid curcular import

--- cogs/config/__init__.py
+++ cogs/config/__init__.py
@@ -1,4 +1,12 @@
-from .configuration import Configuration
+from .sb import Star
+from .prefix import Prefix
+from .reaction import Reaction
+from .welcome import Welcome
+
+
+class Configuration(Prefix, Reaction, Star, Welcome):
+    """ Configuration commands """
+    emoji = "🛠"
 
 
 async def setup(bot):
--- cogs/config/configuration.py
+++ cogs/config/configuration.py
@@ -1,15 +1,6 @@
-from cogs.config.sb import Star
-from cogs.config.prefix import Prefix
-from cogs.config.reaction import Reaction
-from cogs.config.welcome import Welcome
 from discord.ext import commands
 
 
 class ConfigurationBase(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-
-class Configuration(Prefix, Reaction, Star, Welcome):
-    """ Configuration commands """
-    emoji = "🛠"
