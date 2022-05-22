a9350914c73822931309dfa06e531e829a4d7ca3
DvH <88995192+DangVietH@users.noreply.github.com>
2022-05-22 10:35:03 +0700

    tis is de error

--- cogs/configuration/customslash.py
+++ cogs/configuration/customslash.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 
 
 class CustomSlash(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    emoji = "<:slash:977584566967603210>"
+
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.group(invoke_without_command=True,
@@ -12,12 +14,29 @@ class CustomSlash(commands.Cog):
     async def customslash(self, ctx):
         await ctx.send_help(ctx.command)
 
-    @customslash.command(name="Create", description="Create the command")
+    @customslash.command(name="create", description="Create the command")
     @commands.is_owner()
-    async def cslash_create(self, ctx, name, description, *, value):
-        command = app_commands.Command(name=name,
-                                       description=description,
-                                       callback=value,
+    async def cslash_create(self, ctx):
+        questions = [
+            'What is the name of the command?',
+            'What is the description of the command?',
+            'What is the value of the command?'
+        ]
+        answers = []
+        msg = await ctx.send("Let's make sum slash commands")
+
+        def check(user):
+            return user.author == ctx.author and user.channel == ctx.channel
+
+        for question in questions:
+            await msg.edit(content=question)
+            user_msg = await self.bot.wait_for('message', check=check)
+            answers.append(user_msg.content)
+            await ctx.channel.purge(limit=1)
+
+        command = app_commands.Command(name=answers[0],
+                                       description=answers[1],
+                                       callback=answers[2],
                                        guild_ids=[ctx.guild.id])
         self.bot.tree.add_command(command, guild=ctx.guild)
         await ctx.send(f"Command created. To complete the process, do `{ctx.clean_prefix}customslash sync` so they can appear!")
